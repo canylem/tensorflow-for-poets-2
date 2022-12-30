74a39fce58d77d12a526b734861cdecf292f222e
canylem <114537582+canylem@users.noreply.github.com>
2022-12-30 12:18:59 -0500

    Update retrain.py

--- scripts/retrain.py
+++ scripts/retrain.py
@@ -1323,4 +1323,4 @@ if __name__ == '__main__':
       for more information on Mobilenet.\
       """)
   FLAGS, unparsed = parser.parse_known_args()
-  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+  tf.compat.v1.app.run(main=main, argv=[sys.argv[0]] + unparsed)
