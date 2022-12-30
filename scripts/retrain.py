0c85b8211879536fd8aea42c69731cc67445eccf
canylem <114537582+canylem@users.noreply.github.com>
2022-12-30 13:03:57 -0500

    Update retrain.py

--- scripts/retrain.py
+++ scripts/retrain.py
@@ -272,7 +272,7 @@ def create_model_graph(model_info):
   with tf.Graph().as_default() as graph:
     model_path = os.path.join(FLAGS.model_dir, model_info['model_file_name'])
     with gfile.FastGFile(model_path, 'rb') as f:
-      graph_def = tf.GraphDef()
+      graph_def = tf.compat.v1.GraphDef()
       graph_def.ParseFromString(f.read())
       bottleneck_tensor, resized_input_tensor = (tf.graph_util.import_graph_def(
           graph_def,
